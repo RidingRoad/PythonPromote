abdac65447d8c6fb4c1b30f9bcaa3341708d76d3
RidingRoad <RidingRoad@163.com>
2018-06-12 23:34:28 +0800

    modifed the mini web server

--- MiniWebServer/demo_mini_web_server.py
+++ MiniWebServer/demo_mini_web_server.py
@@ -1,4 +1,4 @@
-import socket, re
+import socket
 
 from mini_web_server_response import *
 
@@ -11,29 +11,6 @@ from multiprocessing import Process
 3.回应请求:对请求进行分类,读取相关资源后根据http协议的要求进行封装返回给客户端.
 4.使用多进程实现多任务多个同时访问的能力
 '''
-
-
-def handler_client(cli_socket):
-    # 连接后返回连接请求内容
-    request_content = cli_socket.recv(1024).decode()
-    # 提取请求数据
-    request_line = request_content.splitlines()[0]
-    regex = re.compile(r"\w+ +/([^ ]*) ")
-    try:
-        request_resource = regex.match(request_line).group(1)
-        print(request_resource)
-
-        # 如果后面没跟数据,就返回主页
-        if request_resource == '':
-            cli_socket.send(index())
-            cli_socket.close()
-        else:
-            # 存在返回相应的资源或不存在返回404
-            cli_socket.send(other_file(request_resource))
-            cli_socket.close()
-    except Exception as e:
-        print("error in regular expression")
-        cli_socket.close()
 
 
 def main():
@@ -52,10 +29,12 @@ def main():
     while True:
         # 等待客户端的连接并返回一个专为客户端服务用的套接字
         cli_socket, cli_addr = server_socket.accept()
-        print(cli_addr)
         cli_socket_process = Process(target=handler_client,args=(cli_socket,))
         cli_socket_process.start()
         cli_socket.close()
 
 if __name__ == '__main__':
     main()
+
+
+
--- MiniWebServer/mini_web_server_response.py
+++ MiniWebServer/mini_web_server_response.py
@@ -1,4 +1,26 @@
-import os
+import os, re
+
+
+def handler_client(cli_socket):
+    # 连接后返回连接请求内容
+    request_content = cli_socket.recv(1024).decode()
+    # 提取请求数据
+    request_line = request_content.splitlines()[0]
+    regex = re.compile(r"\w+ +/([^ ]*) ")
+    try:
+        request_resource = regex.match(request_line).group(1)
+
+        # 如果后面没跟数据,就返回主页
+        if '' == request_resource:
+            cli_socket.send(index())
+            cli_socket.close()
+        else:
+            # 存在返回相应的资源或不存在返回404
+            cli_socket.send(other_file(request_resource))
+            cli_socket.close()
+    except Exception as e:
+        print("error in regular expression")
+        cli_socket.close()
 
 
 def index():
@@ -22,7 +44,7 @@ def other_file(file_name):
         response_line = "HTTP/1.1 200 OK\r\n"
         response_header = "Content-Type:text/html\r\n"
         response = response_line + response_header + "\r\n"
-        return response.encode()+data
+        return response.encode() + data
     else:
         response_line = "HTTP/1.1 404 NOT FOUND\r\n"
         response_header = "Content-Type:text/html\r\n"
