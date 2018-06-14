58146813a8ef7eaa4317b04d358e651a7959f839
RidingRoad <RidingRoad@163.com>
2018-06-14 21:59:24 +0800

    modified the jpg problam

--- MiniWebServer/mini_web_server_response.py
+++ MiniWebServer/mini_web_server_response.py
@@ -36,15 +36,16 @@ def index():
 def other_file(file_name):
     file_list = os.listdir("static/")
     if file_name in file_list:
-
         # 读取内容
         with open("static/" + file_name, 'rb') as request_file:
             data = request_file.read()
 
-        response_line = "HTTP/1.1 200 OK\r\n"
-        response_header = "Content-Type:text/html\r\n"
-        response = response_line + response_header + "\r\n"
-        return response.encode() + data
+            response_line = "HTTP/1.1 200 OK\r\n"
+            response_header = "Content-Type:image/jpg\r\n"
+            # response_header = "Content-Type:text/html\r\n"
+            response = response_line + response_header + "\r\n"
+            return response.encode() + data
+
     else:
         response_line = "HTTP/1.1 404 NOT FOUND\r\n"
         response_header = "Content-Type:text/html\r\n"
